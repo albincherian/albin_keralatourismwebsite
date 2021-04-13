cc698db38103ba2d493ed20b4e06a61ed763cf4f
albin <albincherian001@gmail.com>
2021-04-13 16:25:13 +0530

    First Commit : First Version

--- form.js
+++ form.js
@@ -1,6 +1,6 @@
 let email = document.getElementById("email");
 let error = document.getElementById("error");
-function validate(){
+function validateEmail(){
     let regexp = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\.-]+).(.[a-z]{2,3})(.[a-z]{2,3})?$/
     if(regexp.test(email.value)){
         error.innerHTML = "Valid";
@@ -18,7 +18,7 @@ function validate(){
 let phonenumber = document.getElementById("phonenumber");
 let error2 = document.getElementById("error2");
 
-function phno()
+function validatePhone()
 {
   let phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
   if(phoneno.test(phonenumber.value))
@@ -27,31 +27,33 @@ function phno()
 	 }
    else
      {
-	   alert("Not a valid Phone Number");
+	   alert("Not a valid Phone Number!!");
 	   return false;
      }
 }
 
 
-var pwd = document.getElementById("pwd");
-var error1 = document.getElementById("error1");
 
-function CheckPassword()
-{
-    var decimal=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-    if(decimal.test(pwd.value))
-    {
-        alert('Correct, try another...')
-        return true;
-    }
-    else
-    {
-        alert('Wrong...!')
+function passwordstrength() {
+    var strength = document.getElementById('strength');
+    var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+    var mediumRegex = new RegExp("^(?=.{6,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+    var enoughRegex = new RegExp("(?=.{5,}).*", "g");
+    var pwd = document.getElementById("password");
+    if (pwd.value.length == 0) {
+        strength.innerHTML = 'Type Password';
+    } else if (false == enoughRegex.test(pwd.value)) {
+        strength.innerHTML = '<span style="color:red"><b>add more characters</b></span>';
         return false;
-    } 
+    } else if (strongRegex.test(pwd.value)) {
+        strength.innerHTML = '<span style="color:green"><b>Strong!</b></span>';
+        return true;
+    } else if (mediumRegex.test(pwd.value)) {
+        strength.innerHTML = '<span style="color:orange"><b>Medium!</b></span>';
+        return false;
+    } else {
+        strength.innerHTML = '<span style="color:red"><b>Weak!</b></span>';
+        return false;
+    }
 }
-
- 
-
-
 
